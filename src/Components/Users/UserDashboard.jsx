0d58d525d48c7a6a29a671d38bc75db3e1598e1e
Aaron C <aaronc@Elimah-Williams-Air.local>
2025-05-16 23:00:30 -0400

    Added note for clarity

--- src/Components/Users/UserDashboard.jsx
+++ src/Components/Users/UserDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 // import UserPodcastEntries from './UserPodcastEntries'
 
+// have a state watching the log-out button (when created) to erase localstorage with token contain within it localStorage.clear() <-- :).
 export default function UserDashboard() {
     const {id, user_id} = useParams()
   return (
