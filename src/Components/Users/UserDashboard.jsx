60275c5947bd229c90e174fcbe6f656e3ef35f18
Aaron C <aaronc@Elimah-Williams-Air.local>
2025-05-20 07:01:32 -0400

    FEAT: Able to obtain specific user podcastentries on front end. Will work on audio generation next

--- src/Components/Users/UserDashboard.jsx
+++ src/Components/Users/UserDashboard.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-// import UserPodcastEntries from './UserPodcastEntries'
+import UserPodcastEntries from './UserPodcastEntries'
 
 // have a state watching the log-out button (when created) to erase localstorage with token contain within it localStorage.clear() <-- :).
 export default function UserDashboard() {
     const {id, user_id} = useParams()
-    const [podcastentries, setPodcastEntries]=  useState()
     
   return (
     <div>UserDashboard
         <p>User ID: {id}</p>
+        <div>{<UserPodcastEntries user_id ={user_id}/>}</div>
     </div>
   )
 }
--- src/Components/Users/UserPodcastEntries.jsx
+++ src/Components/Users/UserPodcastEntries.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 export default function UserPodcastEntries() {
-
+  const {id} = useParams()
+  const API = import.meta.env.VITE_BASE_URL
+  const [userPodcast, setUserPodcastEntries] = useState()
+  const [error, setError] = useState(null)
+  const [] = useState()
   // This will be the design page for the Podcast Entries
   // Styling, positioning as a component, etc. 
+
+
+  useEffect(()=> {
+    const fetchPodcasts = async () => {
+      const token = localStorage.getItem('token')
+      try {
+        const response = await axios.get(`${API}/users/${id}/podcastentries`, {
+          headers: { Authorization: `Bearer ${token}`}
+        })
+        console.log(response.data)
+        
+      } catch (error) {
+        
+      }
+
+    }
+    fetchPodcasts()
+  },[])
+
+  const deletePodcast = async () => {
+    try {
+      const response = await axios.delete(`${API}/users/${id}/podcastentries`,
+        {headers: { Authorization: `Bearer ${token}`}})
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div>UserPodcastEntries</div>
   )
